2eabe4566769a4ac5b0e6df47bac0cb283f11f9b
yournumberone <marsthegod@protonmail.com>
2022-04-30 21:07:34 +0300

    first task

--- 1.rb
+++ 1.rb
@@ -1,1 +1,15 @@
 # Solution:
+
+range = 1..100
+
+for i in range
+	if i%3 == 0 && i%5 != 0
+		puts 'Hi'
+	elsif i%3 != 0 && i%5 == 0
+		puts 'By'
+	elsif i%3 == 0 && i%5 == 0
+		puts 'HiBy'
+	else
+		puts i
+	end
+end
