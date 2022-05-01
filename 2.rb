e7933af5a889d8b4a09b3f5fdfeaedd5037dcf72
yournumberone <marsthegod@protonmail.com>
2022-05-01 20:32:17 +0300

    Update 2.rb

--- 2.rb
+++ 2.rb
@@ -1,1 +1,18 @@
 # Solution:
+
+string_one = 'HellO WorlD!'
+string_two = 'wHAT dA lANGUAGE?!'
+
+def my_swapcase(string)
+  array = []
+  string.each_char do |char|
+    char == char.upcase ? array << char.downcase : array << char.upcase
+  end
+  array.join
+end
+
+p string_one.swapcase
+p my_swapcase(string_two)
+
+# test
+puts my_swapcase(string_one) == string_one.swapcase
